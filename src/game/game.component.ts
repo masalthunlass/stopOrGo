221f19aea5a48e9faa8d0ad3bd45c5ee9dcdf9e7
masalthunlass <masalthunlass@gmail.com>
2021-08-17 16:59:58 +0200

    added  game component

--- src/game/game.component.ts
+++ src/game/game.component.ts
@@ -1,25 +1,26 @@
-export {Dice} from "./src/dice/dice.component";
-export {RaceTrack} from "./src/race-track/race-track.component";
-import css from "bundle-text:./global.css";
+export {Dice} from "../dice/dice.component";
+export {RaceTrack} from "../race-track/race-track.component";
+import css from "bundle-text:./game.css";
 
 const template = document.createElement('template');
 template.innerHTML = `
     <style>${css}</style>
-   <div id="app">
-        <h2>Stop Or Go ?</h2>
+   <div id="game">
         <sog-dice></sog-dice>
         <sog-race-track length="10"></sog-race-track>
     </div>
 `;
 
-export class Index extends HTMLElement {
+export class GameComponent extends HTMLElement {
     constructor() {
         super();
         this.attachShadow({mode: "open"})
             .appendChild(
                 template.content.cloneNode(true)
             );
+
     }
+
 }
 
-customElements.define('sog-app', Index);
+customElements.define('sog-game', GameComponent);
